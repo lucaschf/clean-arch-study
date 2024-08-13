f32afcfd7a7ee51ab4eb0333d867239aadd06691
Lucas Cristovam <lucas.henriques@totvs.com.br>
2024-08-13 18:06:38 -0300

    refactor: simplify event dispatcher

--- src/domain/__shared/event/event_dispatcher.py
+++ src/domain/__shared/event/event_dispatcher.py
@@ -1,3 +1,6 @@
+import contextlib
+from collections import defaultdict
+
 from .event import Event
 from .event_dispatcher_interface import IEventDispatcher
 from .event_handler_interface import IEventHandler
@@ -8,25 +11,23 @@ class EventDispatcher(IEventDispatcher):
 
     def __init__(self) -> None:
         """Initializes the event dispatcher."""
-        self._handlers = {}
+        self._handlers = defaultdict(list)
 
     def dispatch(self, event: Event) -> None:
         """Dispatches the event to the registered handlers."""
-        handlers = self._handlers.get(event.name(), [])
+        handlers = self._handlers[event.name()]
 
         for handler in handlers:
             handler.handle(event)
 
     def register(self, event_name: str, event_handler: IEventHandler) -> None:
         """Registers a handler for a specific event name."""
-        self._handlers.setdefault(event_name, []).append(event_handler)
+        self._handlers[event_name].append(event_handler)
 
     def unregister(self, event_name: str, event_handler: IEventHandler) -> None:
         """Unregisters a handler for a specific event name."""
-        if event_name not in self._handlers:
-            return
-
-        self._handlers[event_name].remove(event_handler)
+        with contextlib.suppress(ValueError):
+            self._handlers[event_name].remove(event_handler)
 
     def unregister_all(self) -> None:
         """Unregisters all event handlers."""
